9dfe7d45d4dfff78bb87e815b9a57ef4cd7bffd9
IslamHassan94 <islamhasan@hotmail.com>
2023-07-09 22:32:51 +0300

    Renamed main class

--- src/main/java/com/islam/joblisting/JobListingApplication.java
+++ src/main/java/com/islam/joblisting/JobListingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JoblistingApplication {
+public class JobListingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JoblistingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JobListingApplication.class, args);
+    }
 
 }
